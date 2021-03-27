74e89de8a66752d6b94ac551165957ef65dc6a9f
kinsomyjs <FJS0420@outlook.com>
2021-03-27 12:28:43 +0800

    medium148排序链表

---
+++ LeetCode/src/medium139单词拆分.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class medium139单词拆分 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet(wordDict);
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[len];
+    }
+}
