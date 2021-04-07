4fe6bb3d6a4640d8e5a16ef13cd053f1a8bce02f
kinsomyjs <FJS0420@outlook.com>
2021-04-07 18:05:52 +0800

    medium142环形链表II

---
+++ LeetCode/src/medium142环形链表II.java
@@ -0,0 +1,21 @@
+import com.sun.org.apache.xerces.internal.util.SAXLocatorWrapper;
+
+public class medium142环形链表II {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        do {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast);
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
