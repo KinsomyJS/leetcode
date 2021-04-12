873c05b44c04e5f695eb3dbf29289ea17a11808c
kinsomyjs <FJS0420@outlook.com>
2021-04-12 20:23:44 +0800

    easy543二叉树的直径

---
+++ LeetCode/src/easy543二叉树的直径.java
@@ -0,0 +1,18 @@
+public class easy543二叉树的直径 {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        max = Math.max(max, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
