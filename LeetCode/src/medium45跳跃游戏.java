299a646d3e04d88e410cd8a8d36935bc6eae7a40
kinsomyjs <FJS0420@outlook.com>
2021-04-28 19:39:42 +0800

    medium45跳跃游戏

--- LeetCode/src/medium45跳跃游戏.java
+++ LeetCode/src/medium45跳跃游戏.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class medium45跳跃游戏 {
+public class medium45跳跃游戏 {
+    public static int jump(int[] nums) {
+        int len = nums.length;
+        boolean[] reached = new boolean[len];
+        int target = len - 1;
+        int res = 0;
+        while (target > 0) {
+            int index = target;
+            for (int i = target - 1; i >= 0; i--) {
+                if (target - i <= nums[i]) {
+                    index = i;
+                }
+            }
+            target = index;
+            res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        jump(new int[]{2,3,1,1,4});
+    }
 }
