7638381f52731c876c237d324f78e2fbbc09c24a
kinsomyjs <FJS0420@outlook.com>
2021-04-30 15:40:18 +0800

    medium337打家劫舍III

---
+++ LeetCode/src/medium337打家劫舍III.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class medium337打家劫舍III {
+    private Map<TreeNode, Integer> sMap = new HashMap<>();
+    private Map<TreeNode, Integer> nsMap = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(sMap.getOrDefault(root, 0), nsMap.getOrDefault(root, 0));
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        dfs(root.right);
+        sMap.put(root, root.val + nsMap.getOrDefault(root.left, 0) + nsMap.getOrDefault(root.right, 0));
+        nsMap.put(root,
+                Math.max(nsMap.getOrDefault(root.left, 0), sMap.getOrDefault(root.left, 0)) +
+                        Math.max(nsMap.getOrDefault(root.right, 0), sMap.getOrDefault(root.right, 0)));
+    }
+}
